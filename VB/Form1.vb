c2ef8ece2d62baab9578ad18b6352eab5db1a4e9
DevExpressExampleBot <fakeEmail>
2025-02-14 16:51:22 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,20 +1,20 @@
-﻿Imports System.Windows.Forms
 Imports DevExpress.Office.Utils
 Imports DevExpress.XtraEditors
 Imports DevExpress.XtraRichEdit.Services
 
 Namespace RichEditSyntaxSample
-	Partial Public Class Form1
-		Inherits XtraForm
 
-		Public Sub New()
-			InitializeComponent()
-			richEditControl1.Options.Search.RegExResultMaxGuaranteedLength = 500
-			richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New CustomSyntaxHighlightService(richEditControl1.Document))
-			richEditControl1.LoadDocument("CarsXtraScheduling.sql")
-			richEditControl1.ActiveViewType = DevExpress.XtraRichEdit.RichEditViewType.Draft
-			richEditControl1.Document.Sections(0).Page.Width = Units.InchesToDocumentsF(80F)
-			richEditControl1.Document.DefaultCharacterProperties.FontName = "Courier New"
-		End Sub
-	End Class
+    Public Partial Class Form1
+        Inherits XtraForm
+
+        Public Sub New()
+            InitializeComponent()
+            richEditControl1.Options.Search.RegExResultMaxGuaranteedLength = 500
+            richEditControl1.ReplaceService(Of ISyntaxHighlightService)(New CustomSyntaxHighlightService(richEditControl1.Document))
+            richEditControl1.LoadDocument("CarsXtraScheduling.sql")
+            richEditControl1.ActiveViewType = DevExpress.XtraRichEdit.RichEditViewType.Draft
+            richEditControl1.Document.Sections(0).Page.Width = Units.InchesToDocumentsF(80F)
+            richEditControl1.Document.DefaultCharacterProperties.FontName = "Courier New"
+        End Sub
+    End Class
 End Namespace
